602754a5f4ec1c2baff0551f92ef1390c3af5f03
MinhTienSinh <minhprovip123.vai@gmail.com>
2026-04-23 15:23:18 +0700

    Update dieukhien.cpp

--- servo/src/dieukhien.cpp
+++ servo/src/dieukhien.cpp
@@ -1,39 +1,49 @@
+//Khai báo thư viện và biến toàn cục
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
 Servo myservo1, myservo2, myservo3;  // create servo object to control servos
 int pos1 = 0;
 int pos2 = 0;
-int pos3 = 0;   // initial position of the servos
+int pos3 = 0;
 
+//Hàm chỉ chạy đúng 1 lần khi reset hoặc khởi động lại thiết bị
 void setup(){
+   //Khai báo chân GPIO cho các servo
    myservo1.attach(13); // attaches on GPIO 13
    myservo2.attach(12); // attaches on GPIO 12
    myservo3.attach(14); // attaches on GPIO 14
 
+   /*Khởi động Serial để giao tiếp với máy tính qua cổng USB
+   với tốc độ 115200 baud */
    Serial.begin(115200); // monitor speed
    Serial.setTimeout(10000); // 10 giây
 
+   // Thiết lập tần số PWM cho servo (thường là 50 Hz)
    myservo1.setPeriodHertz(50); // standard 50 hz servo
    myservo2.setPeriodHertz(50); // standard 50 hz servo
    myservo3.setPeriodHertz(50); // standard 50 hz servo
 
+   // In ra hướng dẫn sử dụng trên Serial Monitor
    Serial.println("Servo is ready");
    Serial.println("Use format: s1=90 or s2=45 or s3=120"); // instruction
 }
 
+//Hàm chạy liên tục sau khi hàm setup() hoàn thành
 void loop(){
 
-  // check if data is available from Serial
+  //Kiểm tra xem nếu có dữ liệu được gửi từ máy tính qua Serial Monitor thì đọc và xử lý
   if (Serial.available()){
-
-     String s = Serial.readStringUntil('\n'); // read 1 line
-     s.trim(); // remove spaces / newline
-
+      //Đọc dữ liệu từ Serial Monitor cho đến khi gặp ký tự xuống dòng '\n'
+     String s = Serial.readStringUntil('\n');
+     s.trim(); // loại bỏ khoảng trắng đầu cuối nếu có
+     
+      // Biến tạm để lưu số servo và góc điều khiển
      int servoNum = 0; // servo index (1,2,3)
      int angle = 0;    // input angle
 
-     // read format: s1=90
+     /* Sử dụng sscanf để phân tích chuỗi đầu vào theo định
+      dạng "sX=Y" trong đó X là số servo (1-3) và Y là góc (0-180) */
      if (sscanf(s.c_str(), "s%d=%d", &servoNum, &angle) == 2){
 
         // check valid angle
